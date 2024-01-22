592c3f4cbe518850d6dd1745142374791c28a8de
HermanYudin <hermanhockey1994@yandex.ru>
2024-01-22 21:54:07 +0300

    feat:problem with files in folder was resolved

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+const dir = path.resolve(__dirname, 'secret-folder');
+
+fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  files.forEach((el) => {
+    if (el.isFile()) {
+      const file = path.join(dir, el.name);
+
+      fs.stat(file, (err, stats) => {
+        if (err) throw err;
+        const fileInfo = path.parse(file);
+        stdout.write(
+          fileInfo.name +
+            ' - ' +
+            fileInfo.ext.slice(1) +
+            ' - ' +
+            stats.size +
+            '\n',
+        );
+      });
+    }
+  });
+});
